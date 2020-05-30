9e56eae6dedc8dffed19aaf769238b04d8e1273d
YASH2122 <32073855+YASH2122@users.noreply.github.com>
2020-05-30 12:55:18 +0530

    Coronavirus Dashboard

---
+++ js/data_area.js
@@ -0,0 +1,339 @@
+let data=
+[
+    {"Name":"Andaman and Nicobar",
+    "Confirmed" :33,
+    "Active" :0,
+    "Recovered" :33,
+    "Decreased" :0,
+    "lat" :11.66702557,
+    "long":92.73598262
+},
+
+{   "Name":"Andhra Pradesh",
+"Confirmed" :3251,
+"Active" :1067,
+"Recovered" :  2125,
+"Decreased" :59,
+"lat" :14.7504291,
+"long":78.57002559
+},
+
+{   "Name":"Arunachal Pradesh",
+    "Confirmed" :3,
+    "Active" :2,
+    "Recovered" :1,
+    "Decreased" :0,
+    "lat" :27.10039878,
+    "long":93.61660071
+},   
+
+{   "Name":"Assam",
+    "Confirmed" :856,
+    "Active" :748,
+    "Recovered" :104,
+    "Decreased" :4,
+    "lat" :26.7499809,
+    "long":94.21666744
+},
+
+{   "Name":"Bihar",
+    "Confirmed" :3296,
+    "Active" :2070,
+    "Recovered" :1211,
+    "Decreased" :15,
+    "lat" :25.78541445,
+    "long":87.4799727
+},
+
+{   "Name":"Chandigarh",
+    "Confirmed" :288,
+    "Active" :95,
+    "Recovered" :189,
+    "Decreased" :4,
+    "lat" :30.71999697,
+    "long":76.78000565
+},
+
+{ "Name":"Chhattisgarh",
+    "Confirmed" :399,
+    "Active" :316,
+    "Recovered" : 83,
+    "Decreased" :0,
+    "lat" :22.09042035,
+    "long":82.15998734
+},
+
+{ "Name":"Dadra and Nagar Haveli",
+    "Confirmed" :2,
+    "Active" :2,
+    "Recovered" :0,
+    "Decreased" :0,
+    "lat" :20.26657819,
+    "long":73.0166178
+},
+
+{ "Name":"Daman and Diu",
+    "Confirmed" :0,
+    "Active" :0,
+    "Recovered" :0,
+    "Decreased" :0,
+    "lat":20.397373,
+    "long":72.832802
+},
+
+{ "Name":"Delhi",
+    "Confirmed" :16281,
+    "Active" :8470,
+    "Recovered" :7495,
+    "Decreased" :316,
+    "lat" :28.6699929,
+    "long":77.23000403
+},
+
+{ "Name":"Goa",
+    "Confirmed" :69,
+    "Active" :31,
+    "Recovered" :38,
+    "Decreased" :0,
+    "lat" :15.491997,
+    "long":73.81800065
+},
+
+{ "Name":"Gujarat",
+    "Confirmed" :15562,
+    "Active" :6599,
+    "Recovered" :8003,
+    "Decreased" :960,
+    "lat":22.309425,
+    "long":72.136230
+},
+
+{ "Name":"Haryana",
+    "Confirmed" :1504,
+    "Active" :604,
+    "Recovered" :881,
+    "Decreased" :19,
+    "lat" :28.45000633,
+    "long":77.01999101
+},
+
+{ "Name":"Himachal Pradesh",
+    "Confirmed" :276,
+    "Active" :201,
+    "Recovered" :70,
+    "Decreased" :5,
+    "lat" :31.10002545,
+    "long":77.16659704
+},
+
+{ "Name":"Jammu and Kashmir",
+    "Confirmed" :2036,
+    "Active" :1150,
+    "Recovered" :859,
+    "Decreased" :27, 
+    "lat" :34.29995933,
+    "long":74.46665849
+},
+
+{ "Name":"Jharkhand",
+    "Confirmed" :469,
+    "Active" :253,
+    "Recovered" :212,
+    "Decreased" :4,
+    "lat" :23.80039349,
+    "long":86.41998572
+},
+
+{"Name":"Karnataka",
+    "Confirmed" :2533,
+    "Active" :1652,
+    "Recovered" :834,
+    "Decreased" :47,
+    "lat" :12.57038129,
+    "long":76.91999711
+},
+
+{   "Name":"Kerala",
+    "Confirmed" :1088,
+    "Active" :526,
+    "Recovered" :555,
+    "Decreased" :7,
+    "lat" :8.900372741,
+    "long":76.56999263
+},
+
+{ "Name":"Ladakh",
+    "Confirmed" :73,
+    "Active" :30,
+    "Recovered" :43,
+    "Decreased" :0,
+    "lat":34.152588,
+    "long":77.577049
+},
+
+{ "Name":"Lakshadweep",
+    "Confirmed" :0,
+    "Active" :0,
+    "Recovered" :0,
+    "Decreased" :0,
+    "lat" :10.56257331,
+    "long":72.63686717
+},
+
+{ "Name":"Madhya Pradesh",
+    "Confirmed" :7453,
+    "Active" :3082,
+    "Recovered" :4050,
+    "Decreased" :321,
+    "lat" :21.30039105,
+    "long":76.13001949
+},
+
+{ "Name":"Maharashtra",
+    "Confirmed" :59546,
+    "Active" :38948,
+    "Recovered" :18616,
+    "Decreased" :1982,
+    "lat" :19.25023195,
+    "long":73.16017493
+},
+
+{   "Name":"Manipur",
+    "Confirmed" :55,
+    "Active" :50,
+    "Recovered" :5,
+    "Decreased" :0,
+    "lat" :24.79997072,
+    "long":93.95001705
+},
+
+{ "Name":"Meghalaya",
+    "Confirmed" :21,
+    "Active" :8,
+    "Recovered" :12,
+    "Decreased" :1,
+    "lat" :25.57049217,
+    "long":91.8800142
+},
+
+{ "Name":"Mizoram",
+    "Confirmed" :1,
+    "Active" :0,
+    "Recovered" :1,
+    "Decreased" :0,
+    "lat" :23.71039899,
+    "long":92.72001461
+},
+
+{ "Name":"Nagaland",
+    "Confirmed" :18,
+    "Active" :18,
+    "Recovered" :0,
+    "Decreased" :0,
+    "lat" :25.6669979,
+    "long":94.11657019
+},
+
+{ "Name":"Odisha",
+    "Confirmed" :1660,
+    "Active" :766,
+    "Recovered" :887,
+    "Decreased" :7,
+    "lat" :19.82042971,
+    "long":85.90001746
+},
+
+{   "Name":"Puducherry",
+    "Confirmed" :51,
+    "Active" :37,
+    "Recovered" :14,
+    "Decreased" :0,
+    "lat" :11.93499371,
+    "long":79.83000037
+},
+
+{ "Name":"Punjab",
+    "Confirmed" :2158,
+    "Active" :172,
+    "Recovered" :1946,
+    "Decreased" :40,
+    "lat" :31.51997398,
+    "long":75.98000281
+},
+
+{ "Name":"Rajasthan",
+    "Confirmed" :8067,
+    "Active" :3070,
+    "Recovered" :4817,
+    "Decreased" :180,
+    "lat" :26.44999921,
+    "long":74.63998124
+},
+
+{ "Name":"Sikkim",
+    "Confirmed" :1,
+    "Active" :1,
+    "Recovered" :0,
+    "Decreased" :0,
+    "lat" :27.3333303,
+    "long":88.6166475
+},
+
+{ "Name":"Tamil Nadu",
+    "Confirmed" :19372,
+    "Active" :8679,
+    "Recovered" :10548,
+    "Decreased" :145,
+    "lat" :12.92038576,
+    "long":79.15004187
+},
+
+{ "Name":"Telengana",
+    "Confirmed" :2256,
+    "Active" :844,
+    "Recovered" :1345,
+    "Decreased" : 67,
+    "lat":17.123184,
+    "long":79.208824
+},
+
+{ "Name":"Tripura",
+    "Confirmed" :242,
+    "Active" :75,
+    "Recovered" :167,
+    "Decreased" :0,
+    "lat" :23.83540428,
+    "long":91.27999914
+},
+
+{ "Name":"Uttar Pradesh",
+    "Confirmed" :7170,
+    "Active" :2758,
+    "Recovered" :4215,
+    "Decreased" :197,
+    "lat" :27.59998069,
+    "long":78.05000565
+},
+
+{ "Name":"Uttarakhand",
+    "Confirmed" :500,
+    "Active" :417,
+    "Recovered" :79,
+    "Decreased" :4,
+    "lat" :30.32040895,
+    "long":78.05000565
+},
+
+{ "Name":"West Bengal",
+    "Confirmed" :4536,
+    "Active" :2573,
+    "Recovered" :1668,
+    "Decreased" :295,
+    "lat" :22.58039044,
+    "long":88.32994665
+}]
+
+
+
+
+
